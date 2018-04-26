9e2d18c4ebdfc52ec50bf3937d0f47a9407cc184
Jérémy Bourel <calaruni@hotmail.com>
2018-04-26 16:25:27 +0200

    Install serializer

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,19 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Firebase\JWT\JWT;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
     /**
      * @Route("/user", name="user")
      */
-    public function index()
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
+        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+
+        return $this->json(['user' => $users], 200);
     }
 }
