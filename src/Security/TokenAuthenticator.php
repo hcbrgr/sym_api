7c56cc22ce4dfe3615aa27b49df2bf1efbb1e299
Constantin <constantin.burger@hetic.net>
2018-04-27 15:16:54 +0200

    fixing an auth problem

--- src/Security/TokenAuthenticator.php
+++ src/Security/TokenAuthenticator.php
@@ -14,16 +14,7 @@ class TokenAuthenticator extends AbstractGuardAuthenticator
 {
     public function supports(Request $request)
     {
-        if ($request->getUri() === "http://localhost:8000/api/login" || $request->getUri() === "http://localhost:8000/getQRCode" || $request->getUri() === "http://localhost:8000/") {
-
-            return false;
-        }
-        if (!$request->headers->has('X-AUTH-TOKEN')) {
-
-            return new JsonResponse(['error' => "Veuillez envoyer un token"], 404);
-        }
-
-        return true;
+        return $request->headers->has('X-AUTH-TOKEN');
     }
 
     public function getCredentials(Request $request)
