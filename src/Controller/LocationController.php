4b4db6b11a01b0ba6d8810e82224bf9fa23ae9c5
Constantin <constantin.burger@hetic.net>
2018-04-27 09:26:46 +0200

    Adding template and location methods

--- src/Controller/LocationController.php
+++ src/Controller/LocationController.php
@@ -6,11 +6,17 @@ use App\Entity\Location;
 use App\Repository\LocationRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Response\QrCodeResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+
+
 
 class LocationController extends Controller
 {
     /**
-     * @Route("/location", name="location")
+     * @Route("/", name="location")
      */
     public function index(LocationRepository $locationRepository)
     {
@@ -18,7 +24,47 @@ class LocationController extends Controller
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/LocationController.php',
         ]);*/
-        $location = $this->getDoctrine()->getRepository('App:Location')->find(1);
-        return $this->json(['location' => $location]);
+
+        return $this->render('location/index.html.twig', ['pastries' => $locationRepository->findAll()]);
+
+        //$location = $this->getDoctrine()->getRepository('App:Location')->find(1);
+        //return $this->json(['location' => $location]);
+
+        /*$request = Request::createFromGlobals();
+        echo $request->query->get('QRCodeData');
+        if(true){
+            $response = $this->json(['response' => 'OK']);
+        }*/
+    }
+    /**
+     * @Route("/api/checkIn", name="checkin")
+     */
+    public function checkin()
+    {
+        //$location = $this->getDoctrine()->getRepository('App:Location')->find(1);
+        //return $this->json(['location' => $location]);
+
+        $request = Request::createFromGlobals();
+        $request->query->get('QRCodeData');
+        if(true){
+            $response = $this->json(['response' => $request]);
+        }
+
+        return $response;
+    }
+    /**
+     * @Route("/getQRCode", name="getqrcode")
+     */
+    public function qrCode()
+    {
+        $qrLocation = 'Salle 7';
+        $qrDate = date("Y-m-d H:i:s");
+        $qrCode = new QrCode($qrLocation.$qrDate);
+
+        header('Content-Type: '.$qrCode->getContentType());
+        header("Refresh:10");
+        $response = new QrCodeResponse($qrCode);
+
+        return $response;
     }
 }
